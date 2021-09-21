d84c4268a38e4f46408bedcfdbb1006aa3425c8d
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-21 18:26:44 -0500

    Understanding arrays in php

---
+++ fundamentals/arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+echo $weekDays[0]."<br>";
+echo $weekDays[1]."<br>";
+echo $weekDays[2]."<br>";
+echo $weekDays[3]."<br>";
+echo $weekDays[4]."<br>";
+echo $weekDays[5]."<br>";
+echo $weekDays[6]."<br>";
+
+?>
