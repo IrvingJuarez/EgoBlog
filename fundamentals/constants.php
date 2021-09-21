8eab95358376d6f2c44223ecf8a905377de1ce73
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-21 18:01:21 -0500

    Understanding constants using define(name, value)

---
+++ fundamentals/constants.php
@@ -0,0 +1,24 @@
+<?php
+
+define("PI", 3.141529);
+define("NAME", "Irving Vladimir Juarez Cuero")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Variables</h2>
+    <p>Let us initialize a value, but we can change it along the code advance</p>
+    <h2>Constants</h2>
+    <p>They are defined once and they will never change again, otherwise, we would have an error</p>
+    <p>An example of a constant is the PI number, that is <?php echo PI; ?></p>
+    <p>Anyways, we can have a string as a constant too, for example my name will never change, this is <?php echo NAME; ?></p>
+</body>
+</html>
