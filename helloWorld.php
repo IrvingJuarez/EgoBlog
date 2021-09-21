f44a33d44da13dab30e41e0b1e0764baef8a3daf
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-21 17:26:42 -0500

    My first hello world with php

---
+++ helloWorld.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo "Hello World";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><?php echo "Hi, I am a h2 tag written from php code"; ?></h2>
+</body>
+</html>
