fffc1d9d3afbd2583b32b708fee45cf140e26a7b
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-28 11:04:24 -0500

    Completing get lesson

--- forms/get.php
+++ forms/get.php
@@ -4,8 +4,15 @@ if(!$_GET){
     header("Location: http://localhost/LiberMeus/forms/structure.php");
 }
 
-$name = $_GET["name"];
+$name = htmlspecialchars($_GET["name"]);
+$gender = htmlspecialchars($_GET["gender"]);
+$country = htmlspecialchars($_GET["country"]);
+$terms = htmlspecialchars($_GET["conditions"]) || false;
 
-echo "Your name is ".htmlspecialchars($name);
+echo "Your name is ".$name.", you live in ".$country.", and you are a ".$gender;
+echo "<br>";
+if($terms){
+    echo "You accepted the terms and conditions";
+}
 
 ?>
