838b891bf10ed07b378712ef0d25351dcce49b56
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-28 22:06:49 -0500

    Styles and structure of my first practice in php

---
+++ practices/contact-form/index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+
+        <label for="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+        <input type="submit" value="Send message" name="submit" class="btn-submit">
+    </form>
+</body>
+</html>
