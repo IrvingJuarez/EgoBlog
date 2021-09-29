f0f1ae68433405334ec2ccfffa3d55769aaa2f1b
Irving Juarez <irvingjuarez274@gmail.com>
2021-09-28 20:57:44 -0500

    Learning about how to validate text fields

--- forms/form.php
+++ forms/form.php
@@ -1,3 +1,41 @@
+<?php
+
+$error = "";
+
+function getVarReady($var){
+    $var = filter_var($var, FILTER_SANITIZE_STRING);
+    $var = trim($var);
+    $var = htmlspecialchars($var);
+    $var = stripslashes($var);
+    return $var;
+}
+
+if(isset($_POST['basic-info'])){
+    $name = $_POST['name'];
+
+    if(!empty($name)){
+        $name = getVarReady($name);
+        echo "Your name is ".$name."<br>";
+    }else{
+        $error .= "Warning: Please add a name <br>";
+    }
+
+    $email = $_POST['email'];
+
+    if(!empty($email)){
+        $email = getVarReady($email);
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo "Your email is: ".$email."<br>";
+        }else{
+            $error .= "Warning: Your email address is not valid <br>";
+        }
+    }else{
+        $error .= "Warning: Please add an email address <br>";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +45,17 @@
     <title>Form</title>
 </head>
 <body>
-    <form action="verificator.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         <input type="text" name="name" id="name" placeholder="Name">
         <br>
-        <input type="text" name="email" id="email" placeholder="Email">
+        <input type="email" name="email" id="email" placeholder="Email">
         <br>
         <input type="submit" value="Send" name="basic-info">
     </form>
+
+    <?php if(!empty($error)): ?>
+        <h3><?php echo $error; ?></h3>
+    <?php endif; ?>
+
 </body>
 </html>
