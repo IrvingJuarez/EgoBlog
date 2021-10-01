02a76693a2a8bebe74277042172134adb5f67333
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-01 17:06:45 -0500

    I learned how to connect to a db using the Prepared Statements method

--- pdo/index.php
+++ pdo/index.php
@@ -1,15 +1,22 @@
 <?php
 
-$theId = $_GET['id'];
+$theId = $_GET['id'] ?? 2;
 
 try{
     $connection = new PDO('mysql:host=localhost:8111;dbname=second_test', 'root', '');
-    // echo "Successful connection to the database";
 
-    $result = $connection->query('SELECT * FROM users WHERE id = '.$theId);
+    // $result = $connection->query('SELECT * FROM users WHERE id = '.$theId);
 
-    foreach($result as $i){
-        echo "The name is: ".$i['name']." and the id: ".$i['id']."<br>";
+    // foreach($result as $i){
+    //     echo "The name is: ".$i['name']." and the id: ".$i['id']."<br>";
+    // }
+
+    $query = $connection->prepare("SELECT * FROM users");
+    $query->execute();
+
+    $result = $query->fetchAll();
+    foreach($result as $item){
+        echo "The name is ".$item['name']." and the id is ".$item['id']."<br>";
     }
 }catch(PDOException $e){
     echo "Error: ".$e->getMessage();
