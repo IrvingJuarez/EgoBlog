03bc2a29e2fa1d6f306d3d1a53fb6f47185c9eea
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-01 15:39:30 -0500

    Learning how to use Query method in php

--- pdo/index.php
+++ pdo/index.php
@@ -1,8 +1,16 @@
 <?php
 
+$theId = $_GET['id'];
+
 try{
     $connection = new PDO('mysql:host=localhost:8111;dbname=second_test', 'root', '');
-    echo "Successful connection to the database";
+    // echo "Successful connection to the database";
+
+    $result = $connection->query('SELECT * FROM users WHERE id = '.$theId);
+
+    foreach($result as $i){
+        echo "The name is: ".$i['name']." and the id: ".$i['id']."<br>";
+    }
 }catch(PDOException $e){
     echo "Error: ".$e->getMessage();
 }
