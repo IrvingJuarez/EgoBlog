5dc0c4e258059ea99ac08fb73fb6ec94acc68689
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-02 14:32:33 -0500

    Html structure for pagination exercise

---
+++ practices/pagination.php/index.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagination practice</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <main>
+        <h2>Articles</h2>
+        <section class="articles">
+            <ul>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>2.- Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>3.- Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>4.- Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>5.- Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            </ul>
+        </section>
+
+        <section class="pagination">
+            <ul>
+                <li class="disabled"><a href="#">&laquo;</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">&raquo;</a></li>
+            </ul>
+        </section>
+    </main>
+</body>
+</html>
