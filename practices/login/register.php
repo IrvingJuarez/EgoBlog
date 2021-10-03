2ad68b8a6ee59fd64ddf105e496eab8b78be8f82
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-03 14:22:28 -0500

    Comprobation done

--- practices/login/register.php
+++ practices/login/register.php
@@ -1,3 +1,32 @@
 <?php
 
+session_start();
+
+if(isset($_SESSION['user'])){
+    header("Location: index.php");
+}
+
+$errors = "";
+
+function clean($variable){
+    $variable = strtolower($variable);
+    $variable = filter_var($variable, FILTER_SANITIZE_STRING);
+    return $variable;
+}
+
+function emptiness($variable, $msg){
+    if(empty($variable)){
+        global $errors;
+        $errors .= "<span>Please add the <i>$msg</i></span>";
+    }
+}
+
+if(isset($_POST['submit'])){
+    $username = clean($_POST['name']);
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+
+    // echo "$username, $password, $password2";
+}
+
 require "views/register.view.php";
--- practices/login/views/register.view.php
+++ practices/login/views/register.view.php
@@ -4,30 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Sign in</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <main>
         <h2>Registrate</h2>
         <section class="main_form">
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
                 <label for="name">User name</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" id="name" value="<?php echo $username ?? ""; ?>">
 
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" id="password" value="<?php echo $password ?? ""; ?>">
 
                 <label for="password2">Repeat the password</label>
-                <input type="password" name="password2" id="password2">
+                <input type="password" name="password2" id="password2" value="<?php echo $password2 ?? ""; ?>">
 
                 <?php
                     if(isset($_POST['submit'])){
-                        // $name = $_POST['name'];
-                        // $password = $_POST['password'];
+                        emptiness($username, "Username");
+                        emptiness($password, "Password");
+                        emptiness($password2, "Password comprobation");
 
-                        // echo emptiness($name, "User name");
-                        // echo emptiness($password, "Password");
+                        if($errors){
+                            echo $errors;
+                        }else{
+                            echo "<span>The data was sent sucessfully</span>";
+                        }
                     }
                 ?>
 
