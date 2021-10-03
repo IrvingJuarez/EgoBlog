d5404108571258951e6f5107f556ff3e7fbb4b02
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-03 00:47:21 -0500

    Structure of the login practice

---
+++ practices/login/index.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h2>Registrate</h2>
+        <section class="main_form">
+            <form>
+                <label for="name">User name</label>
+                <input type="text" name="name" id="name">
+
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+
+                <label for="password2">Repeat the password</label>
+                <input type="password" name="password2" id="password2">
+
+                <input type="submit" name="submit" value="Log in">
+            </form>
+        </section>
+    </main>
+</body>
+</html>
