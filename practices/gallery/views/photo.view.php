e6edaac9088d735315a46db9de3fd4ef2b703100
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-04 15:02:16 -0500

    The structure for the photo page is done

---
+++ practices/gallery/views/photo.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        Photo Gallery
+    </header>
+    <main>
+        <section class="photo-content">
+            <article class="photo-content_img">
+
+            </article>
+
+            <article class="photo-content_description">
+                <h2>Title</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid fuga, et corrupti commodi deserunt nobis, eum nostrum repellat, eos distinctio temporibus facilis minima reiciendis. Ducimus recusandae pariatur provident adipisci!
+                </p>
+            </article>
+        </section>
+    </main>
+    <footer>
+        A digital product made by: Irving Juárez
+    </footer>
+</body>
+</html>
