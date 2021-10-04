9cb6f4c83d32fad7d25bcfb6a5cbe8f20dc20d1a
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-04 13:24:25 -0500

    Html and css already done

---
+++ practices/gallery/views/index.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Photo gallery</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        Photo Gallery
+    </header>
+    <main>
+        <section class="main-content">
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+        </section>
+    </main>
+    <footer>
+        A digital product made by: Irving Juárez
+    </footer>
+</body>
+</html>
