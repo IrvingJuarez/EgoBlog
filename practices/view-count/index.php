3c65502251a4e49c0f5f82d31fc25304940e2e66
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-04 12:01:02 -0500

    The program already workd finely, but there is left to do some optimization

--- practices/view-count/index.php
+++ practices/view-count/index.php
@@ -1,15 +1,33 @@
 <?php
 
+$value = 0;
+
+function come($connection){
+    global $value;
+    $sql = "SELECT counter FROM views";
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+
+    $result = $statement->get_result();
+    $value = $result->fetch_assoc()['counter'];
+}
+
+function go($connection){
+    global $value;
+    $value += 1;
+    $sql = "UPDATE views SET counter = $value";
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+}
+
 function counter(){
+    global $value;
     $connection = new mysqli('localhost', 'root', '', 'counting', 8080);
-
+    
     if($connection->errno == 0){
-        $sql = "SELECT counter FROM views";
-        $statement = $connection->prepare($sql);
-        $statement->execute();
-
-        $result = $statement->get_result();
-        echo "<h2>The number of views in this website are: <i>".$result->fetch_assoc()['counter']."</i></h2>";
+        come($connection);
+        go($connection);
+        echo "<h2>The number of views in this website are: <i>".$value."</i></h2>";
     }else{
         echo "<h2>There was a problem with the database</h2>";
     }
