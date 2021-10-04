83901bf115aef8e7c7847ed013ecf41cdab3989f
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-04 11:52:03 -0500

    The fetch to the database has been successful

--- practices/view-count/index.php
+++ practices/view-count/index.php
@@ -4,7 +4,12 @@ function counter(){
     $connection = new mysqli('localhost', 'root', '', 'counting', 8080);
 
     if($connection->errno == 0){
-        echo "<h2>The current webpage has <i>$result</i> views</h2>";
+        $sql = "SELECT counter FROM views";
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+
+        $result = $statement->get_result();
+        echo "<h2>The number of views in this website are: <i>".$result->fetch_assoc()['counter']."</i></h2>";
     }else{
         echo "<h2>There was a problem with the database</h2>";
     }
