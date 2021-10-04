21e8505e5b208a7f20faf6e8fe93cf3b10d614e3
Irving Juarez <irvingjuarez274@gmail.com>
2021-10-04 11:14:18 -0500

    Basic structure for the view-counter practice

---
+++ practices/view-count/index.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Views count</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <main>
+        <header>
+            <h2>View counter</h2>
+        </header>
+        <section class="view-container">
+            <h2>This webpage had been seen <i>0</i> times</h2>
+        </section>
+    </main>
+</body>
+</html>
